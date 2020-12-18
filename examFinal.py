28cedf5cf61a32761c3dcffa7bf4cf22866de3e9
tommyokk <70242731+tommyokk@users.noreply.github.com>
2020-12-17 19:26:31 -0800

    Update examFinal.py

--- examFinal.py
+++ examFinal.py
@@ -3,16 +3,6 @@
 
 
 """
-We are trying to show the the number of grades, the average grade,
-and the percentage of grades that are above the average with
-our code
-Open the file "Final.txt" to get the grades. Copy and paste information.
-Average the grades by sum of grades / # of grades.
-If the grade is above average, += 1
-
-"""
-
-"""
 Open Final.txt infile
 grades = line infile
 clse infile
@@ -27,19 +17,3 @@ print("Percentage of grades above average: % above average)
 
 
 """
-
-## Analyze the grades on a final exam.
-infile = open("Final.txt", 'r')
-grades = [line.rstrip()for line in infile]
-infile.close()
-for i in range(len(grades)):
-    grades[i] = int(grades[i])
-average = sum(grades) / len(grades)
-num = 0    # number of grades above average
-for grade in grades:
-    if grade > average:
-        num += 1
-print("Number of grades:", len(grades))
-print("Average grade:", average)
-print("Percentage of grades above average:  {0:2f}%")
-format(100 * num / len(grades))
